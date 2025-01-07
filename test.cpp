18ccdd59c78d9e1d761499b9247dcbf5f432d265
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-08 00:06:46 +0530

    .

--- test.cpp
+++ test.cpp
@@ -2,10 +2,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-  string name ="ravi";
-  name.append("singh");
-
-cout<<name<<"display";
-
-    return 0;
+string name = "ravisingh";
+cout<<*name.end();
+return 0;
 }
