832de2ebfb36def67b6425698fdb4a9a4e7cdf5d
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-09 23:46:25 +0530

    .

--- pt1.cpp
+++ pt1.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"enter the umber of row";
+    cin>>n;
+    for(int i =0; i<n;i++){
+        for(int j =0;j<n ; j++){
+            cout<<"   "<<i;
+        }
+        cout<<"\n";
+    }
     return 0;
 }
