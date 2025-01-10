4080cdc4780a094df62aab1af3f4646f2746d01c
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-10 21:43:42 +0530

    .

--- pt1.cpp
+++ pt1.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>n;
     for(int i =0; i<n;i++){
         for(int j =0;j<n ; j++){
-            cout<<"   "<<i;
+            cout<<"  this is vall of i "<<i;
         }
         cout<<"\n";
     }
