6e635876fb51bcd4e16f87ea6d41f05453f4f9c7
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-18 19:13:05 +0530

    .

--- diamondPattern.cpp
+++ diamondPattern.cpp
@@ -16,14 +16,15 @@ int main(){
         cout<<"\n";
     }
     //lower tringle
-    for(int i =0; i<n;i++){
-        for(int j=n-1-i;j>0;j--){
+    for(int i =0; i<n-1;i++){
+         for(int k=0;k<=i;k++){
             cout<<"_";
+        }
+        for(int j=n-1-i;j>0;j--){
+            cout<<"* ";
 
         }
-        for(int k=0;k<=i;k++){
-            cout<<"* ";
-        }
+       
         cout<<"\n";
     }
     return 0;
