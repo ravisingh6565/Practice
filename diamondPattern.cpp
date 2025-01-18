bdb09570814e6e96e725d2db1a9d1a13c14d739c
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-18 19:11:14 +0530

    .

---
+++ diamondPattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter the number : ";
+    cin>>n;
+
+    for(int i =0; i<n;i++){
+        for(int j=n-1-i;j>0;j--){
+            cout<<"_";
+
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    //lower tringle
+    for(int i =0; i<n;i++){
+        for(int j=n-1-i;j>0;j--){
+            cout<<"_";
+
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
