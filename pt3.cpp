58d9702d0535189407de1380c0acfbb597ae0573
ravisuneel6565 <ravisuneel6262@gmail.com>
2025-01-18 19:07:22 +0530

    .

--- pt3.cpp
+++ pt3.cpp
@@ -1,1 +1,19 @@
-#
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter the number : ";
+    cin>>n;
+
+    for(int i =0; i<n;i++){
+        for(int j=n-i;j>0;j--){
+            cout<<" ";
+
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
